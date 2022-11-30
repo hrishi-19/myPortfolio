d73f1b7b51c68df67cff466530306d695152eb82
hrishi-19 <hrishakamath.com>
2022-11-30 16:06:30 +0530

    crub msg

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,7 +3,7 @@ import React from "react";
 function Contact(){
     return(
         <div className=" home w-3/5 h-5/6 overflow-auto">
-        <h1>About</h1>
+        <h1 className="text-white text-xl">Comming soon</h1>
         </div>
     )
 }
